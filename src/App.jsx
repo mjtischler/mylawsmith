d19097ac3ceebe3659fcf09a38d648af43a5f2b3
Matt Tischler <matt@mylawsmith.com>
2017-05-10 00:40:21 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import LawSmithAppBar from './common/AppBar';
+import Home from './home/Home';
+import './App.css';
+
+const App = () => (
+    <MuiThemeProvider>
+        <div>
+            <LawSmithAppBar />
+            <Home />
+        </div>
+    </MuiThemeProvider>
+);
+
+export default App;
---
+++ src/home/Home.jsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import './Home.css';
+
+const Home = () => (
+    <div className="home-container">
+        <Card className="home-card">
+            <CardHeader
+              title="Without Avatar"
+              subtitle="Subtitle"
+              actAsExpander={true}
+              showExpandableButton={true}
+            />
+
+            <CardActions>
+                <FlatButton label="Action1" />
+                <FlatButton label="Action2" />
+            </CardActions>
+
+            <CardText expandable={true}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+        </Card>
+
+        <Card className="home-card">
+            <CardHeader
+              title="Without Avatar"
+              subtitle="Subtitle"
+              actAsExpander={true}
+              showExpandableButton={true}
+            />
+
+            <CardActions>
+                <FlatButton label="Action1" />
+                <FlatButton label="Action2" />
+            </CardActions>
+
+            <CardText expandable={true}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+        </Card>
+
+        <Card className="home-card">
+            <CardHeader
+              title="Without Avatar"
+              subtitle="Subtitle"
+              actAsExpander={true}
+              showExpandableButton={true}
+            />
+
+            <CardActions>
+                <FlatButton label="Action1" />
+                <FlatButton label="Action2" />
+            </CardActions>
+
+            <CardText expandable={true}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+        </Card>
+
+        <Card className="home-card">
+            <CardHeader
+              title="Without Avatar"
+              subtitle="Subtitle"
+              actAsExpander={true}
+              showExpandableButton={true}
+            />
+
+            <CardActions>
+                <FlatButton label="Action1" />
+                <FlatButton label="Action2" />
+            </CardActions>
+
+            <CardText expandable={true}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+        </Card>
+    </div>
+);
+
+
+export default Home;
